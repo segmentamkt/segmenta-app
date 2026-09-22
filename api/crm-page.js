41d117990b0ece1b3129bf8a054d85b02131adf8
segmentamkt <segmentamarketing180@gmail.com>
2026-09-22 12:27:53 -0500

    Load CRM execution engine UI

--- api/crm-page.js
+++ api/crm-page.js
@@ -5,7 +5,8 @@ module.exports=async function handler(req,res){
     const r=await fetch(`${proto}://${host}/crm-v3.html`);
     if(!r.ok)return res.status(r.status).send('CRM unavailable');
     let html=await r.text();
-    html=html.replace('</head>','<link rel="stylesheet" href="/integrations-ui.css?v=20260922-1">\n</head>');
+    html=html.replace('</head>','<link rel="stylesheet" href="/integrations-ui.css?v=20260922-1">\n<link rel="stylesheet" href="/crm-execution-ui.css?v=20260922-1">\n</head>');
+    html=html.replace('</body>','<script src="/crm-execution-ui.js?v=20260922-1"></script>\n</body>');
     res.setHeader('Content-Type','text/html; charset=utf-8');
     res.setHeader('Cache-Control','no-store');
     return res.status(200).send(html);
